5358e5ab001e418b53bf69731db5c28fd9e8ea1e
mbhmishu <mbhmishu@gmail.com>
2023-11-24 02:19:47 +0600

    static path set

--- softwarechamber/settings.py
+++ softwarechamber/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 
 
